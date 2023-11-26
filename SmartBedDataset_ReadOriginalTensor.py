5a890c78803f13afe6f413e610ef6dc9eb59f1b2
Chenxi Zhao <chenxi.zhao@tum.de>
2023-11-26 15:49:22 +0100

    split SmartBedDataset class to SmartBedDataset_Base and SmartBedDataset_Train (70% for train and 30% for validation)

--- SmartBedDataset_ReadOriginalTensor.py
+++ SmartBedDataset_ReadOriginalTensor.py
@@ -1,13 +1,17 @@
 import torch
 from torch.utils.data import Dataset
 import os
+import random
 
-class SmartBedDataset(Dataset):
+random.seed(21339)
+
+
+class SmartBedDataset_Base(Dataset):
     TRAIN = 0
     TEST = 2
 
     def __init__(self, mode=TRAIN, dataDir="./dataset/saved_tensor_for_train",
-                 dataDirTest="./dataset/saved_tensor_for_test"):
+                 dataDirTest="./dataset/saved_tensor_for_test", time_step=10):
         if not (mode == self.TRAIN or mode == self.TEST):
             print("Error - SmartBedDataset invalid mode " + format(mode))
             exit(1)
@@ -21,7 +25,7 @@ class SmartBedDataset(Dataset):
 
         self.all_inputs, self.all_targets = self.TensorLoader()
 
-        self.length_sque = 10
+        self.length_sque = time_step
         self.length_each_file = [(file.shape[0] - self.length_sque + 1) for file in self.all_inputs]
         self.totalLength = sum(self.length_each_file)
 
@@ -68,16 +72,53 @@ class SmartBedDataset(Dataset):
         return denormalized_data
 
 
-if __name__ == '__main__':
-    data_set = SmartBedDataset()
-    print(data_set[0][0].shape)  # torch.Size([10, 12, 32, 64])
-    print(data_set[0][1].shape)  # torch.Size([1, 32, 64])
+class SmartBedDataset_Train:
+    # for divide train data into train and validation (70% data in one file for train)
+    def __init__(self, ori_dataset, model):
+        self.data_set = ori_dataset
+        train_ratio = 0.7
+        if not os.path.exists("./dataset/for_train/idx_train") and not os.path.exists("./dataset/for_train/idx_vali"):
+            print("Warning: Idx files not found. New files will be generated!!!")
+            index = [i for i in range(len(ori_dataset))]
+            random.shuffle(index)
+            end_index = int(train_ratio * len(ori_dataset))
+            train_idxs = index[0:end_index]
+            vali_idxs = index[end_index:]
+            torch.save(train_idxs, "./dataset/for_train/idx_train")
+            torch.save(vali_idxs, "./dataset/for_train/idx_vali")
+        else:
+            train_idxs = torch.load("./dataset/for_train/idx_train")
+            vali_idxs = torch.load("./dataset/for_train/idx_vali")
 
-    dataset_length = len(data_set)
+        if model == "train":
+            self.idxs = train_idxs
+        elif model == "validation":
+            self.idxs = vali_idxs
+        else:
+            raise NotImplementedError("Wrong Model!")
+
+        # test code
+        # print(len(self.idxs))
+        # print(self.idxs[0: 10])  # [38888, 23892, 8279, 33106, 46281, 7471, 24002, 40308, 18404, 2836]
+
+    def __len__(self):
+        return len(self.idxs)
+
+    def __getitem__(self, idx):
+        return self.data_set[self.idxs[idx]]
+
+
+if __name__ == '__main__':
+    raw_dataset = SmartBedDataset_Base()
+    print(raw_dataset[0][0].shape)  # torch.Size([10, 12, 32, 64])
+    print(raw_dataset[0][1].shape)  # torch.Size([1, 32, 64])
+
+    dataset_length = len(raw_dataset)
     print("Dataset length:", dataset_length)
 
     # Verify if the printed length matches the expected total length
-    if dataset_length == data_set.totalLength:
+    if dataset_length == raw_dataset.totalLength:
         print()
         print("Length check passed!")
 
+    train_dataset = SmartBedDataset_Train(raw_dataset, model="train")
